0e41da54c0a394c66f672135c3a0d8774db3c9d7
hbrausen <hbrausen@ualberta.ca>
2014-07-03 14:02:00 -0600

    Create directories.
    Add test for Bellman-Ford algorithm.

--- bellmanford/bellmanford.cpp
+++ bellmanford/bellmanford.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <set>
 #include <utility>
 #include <limits>
 
@@ -14,11 +15,19 @@ typedef vector<WeightedEdge>::const_iterator WeightedGraphIterator;
 
 struct Graph {
 	vector<WeightedEdge> edges;
+	set<int> nodes;
+	void AddEdge(int a, int b, int w)
+	{
+		nodes.insert(a);
+		nodes.insert(b);
+		edges.push_back(make_pair(Edge(a,b), w));
+	}
+	void AddBiEdge(int a, int b, int w) { AddEdge(a,b,w); AddEdge(b,a,w); }
 };
 
 vector<int> BellmanFord(const Graph& G, int source)
 {
-	vector<int> dist(G.edges.size(), numeric_limits<int>::max());
+	vector<int> dist(G.nodes.size(), numeric_limits<int>::max());
 	dist[source] = 0;
 	for (int i = 0; i < G.edges.size()-1; ++i)
 		for (WeightedGraphIterator it = G.edges.begin(); it != G.edges.end(); ++it)
@@ -31,9 +40,4 @@ bool HasNegativeCycle(const Graph& G, const vector<int>& dist)
 	for (WeightedGraphIterator it = G.edges.begin(); it != G.edges.end(); ++it)
 		if (dist[it->first.a] + it->second < dist[it->first.b]) return true;
 	return false;
-}
-
-int main(int argc, char **argv)
-{
-	return 0;
 }
---
+++ bellmanford/test.cpp
@@ -0,0 +1,17 @@
+#include "bellmanford.cpp"
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	Graph G;
+	G.AddEdge(0, 1, 1);
+	G.AddEdge(1, 2, 1);
+	G.AddEdge(2, 3, 1);
+	G.AddEdge(3, 0, -3);
+	vector<int> dists = BellmanFord(G, 0);
+	for (int i = 0;i < dists.size(); ++i)
+		cout << i << '\t' << dists[i] << endl;
+	cout << "Negative cycle:\t" << HasNegativeCycle(G, dists) << endl;
+	return 0;
+}
